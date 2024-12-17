dfeb3a2b6459ff61ffd634801727b08a85e563fb
Blackerd <nguyenquyduong0802@gmail.com>
2024-12-17 21:53:39 +0700

    update

--- assets/scritp/WebSocketManager.ts
+++ assets/scritp/WebSocketManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, director } from 'cc';
+import { _decorator, Component, director, error } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('WebSocketManager')
